fixed bug with computation of spatial scores

train_spaceenv.py
@@ -53,6 +53,8 @@ def main(cfg: DictConfig):
     # graph
     logging.info(f"Reading graph from {graph_path}.")
     graph = nx.read_graphml(graph_path)
+    node2ix = {n: i for i, n in enumerate(df.index)}
+    edge_list = np.array([(node2ix[e[0]], node2ix[e[1]]) for e in graph.edges])
 
     # === Read covariate groups ===
     if spaceenv.covariates is not None:
@@ -190,13 +192,16 @@ def main(cfg: DictConfig):
     mu = predictor.predict(df)
     mu.name = mu.name + "_pred"
 
-    # synsthetic outcome
+    # sythetic outcome
     logging.info(f"Generating synthetic residuals for synthetic outcome.")
     mu_synth = predictor.predict(df)
-    residuals = df[spaceenv.outcome] - mu_synth
-    synth_residuals, residual_corr = utils.generate_noise_like(residuals, graph)
+    residuals = (df[spaceenv.outcome] - mu_synth).values
+    synth_residuals = utils.generate_noise_like(residuals, edge_list)
     Y_synth = predictor.predict(df) + synth_residuals
     Y_synth.name = "Y_synth"
+
+    residual_smoothness = utils.moran_I(residuals, edge_list)
+    synth_residual_smoothness = utils.moran_I(synth_residuals, edge_list)
 
     # === Counterfactual generation ===
     logging.info(f"Generating counterfactual predictions and adding residuals")
@@ -320,7 +325,7 @@ def main(cfg: DictConfig):
         leave_out_predictor = TabularPredictor(label=spaceenv.outcome)
         leave_out_predictor = leave_out_predictor.fit(
             train_data[cols],
-            **spaceenv.autogluon.fit,
+            **spaceenv.autogluon.leave_out_fit,
             tuning_data=tuning_data[cols],
             use_bag_holdout=True,
         )
@@ -361,9 +366,8 @@ def main(cfg: DictConfig):
     # === Compute the spatial smoothness of each covariate
     logging.info(f"Computing spatial smoothness of each covariate.")
     moran_I_values = {}
-    adjmat = nx.adjacency_matrix(graph, nodelist=df.index).toarray()
     for c in covariates:
-        moran_I_values[c] = utils.moran_I(df[c], adjmat)
+        moran_I_values[c] = utils.moran_I(df[c].values, edge_list)
 
     # === Save results ===
     logging.info(f"Saving synthetic data, graph, and metadata")
@@ -390,7 +394,8 @@ def main(cfg: DictConfig):
         "covariates": list(covariates),
         "treatment_values": avals.tolist(),
         "covariate_groups": covar_groups,
-        "original_residual_spatial_score": float(residual_corr),
+        "original_residual_spatial_score": float(residual_smoothness),
+        "synthetic_residual_spatial_score": float(synth_residual_smoothness),
     }
 
     # save metadata and resolved config

utils.py
@@ -10,6 +10,8 @@ import numpy as np
 import pandas as pd
 from omegaconf.listconfig import ListConfig
 from scipy.linalg import cholesky, solve_triangular
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import splu
 
 
 def scale_variable(
@@ -132,58 +134,93 @@ def generate_noise_like_by_penalty(x: pd.Series, graph: nx.Graph) -> np.ndarray:
     return noise
 
 
-def generate_noise_like(x: pd.Series, graph: nx.Graph) -> np.ndarray:
+def generate_noise_like(
+    x: np.ndarray, edge_list: np.ndarray, attempts: int = 10
+) -> np.ndarray:
     """Injects noise into residuals using a Gaussian Markov Random Field."""
-    # find average correlation between a point and its neighbors mean
-    nbrs2ix = {n: i for i, n in enumerate(x.index)}
-    nbrs = [[nbrs2ix[i] for i in nx.neighbors(graph, n)] for n in x.index]
-    x_ = x.values
-    nbr_means = np.array([np.nanmean(x_[nbrs[i]]) for i in range(len(x))])
-    x__ = (x_ - np.nanmean(x_)) / np.nanstd(x_)
-    nbr_means_ = (nbr_means - np.nanmean(nbr_means)) / np.nanstd(nbr_means)
-    corr = np.nansum(x__ * nbr_means_) / len(x)
+    n = len(x)
 
-    # scaled graph laplacian
-    Q = nx.laplacian_matrix(graph).toarray() / np.mean([len(n) for n in nbrs])
+    # nbrs2ix = {n: i for i, n in enumerate(x.index)}
+    # nbrs = [[nbrs2ix[i] for i in nx.neighbors(graph, n)] for n in x.index]
+    # x_ = x.values
+    nbrs = [[] for _ in range(edge_list.shape[0])]
+    for i, j in edge_list:
+        nbrs[i].append(j)
+        nbrs[j].append(i)
 
-    # add variance to diagonal
-    Q = corr * Q + (1 - corr + 1e-4) * np.eye(Q.shape[0])
-    logging.info(f"Residual neighbor correlation: {corr:.4f}")
+    nbrs_means = np.array([np.nanmean(x[nbrs[i]]) for i in range(n)])
+    nbrs_means[np.isnan(nbrs_means)] = np.nanmean(nbrs_means)
+    x_ = x.copy()
+    x_[np.isnan(x_)] = nbrs_means[np.isnan(x_)]
+    rho = np.corrcoef(x_, nbrs_means)[0, 1]
 
-    # sample from GMRF
-    Z = np.random.randn(Q.shape[0])
-    L = cholesky(Q, lower=True)
-    noise = solve_triangular(L, Z, lower=True).T
+    # 1. Build precision matrix
+    # Arrays to hold the data, row indices, and column indices for Q
+    data = []
+    rows = []
+    cols = []
+
+    # Off-diagonal entries and compute degree for diagonal
+    degree = np.zeros(n)
+    for i, j in edge_list:
+        data.extend([-rho, -rho])
+        rows.extend([i, j])
+        cols.extend([j, i])
+        degree[i] += 1
+        degree[j] += 1
+
+    # Add diagonal entries
+    data.extend(degree)
+    rows.extend(range(n))
+    cols.extend(range(n))
+
+    # build precision matrix
+    Q = csc_matrix((data, (rows, cols)), shape=(n, n))
+    factorization = splu(Q)
+
+    best_result = np.inf
+    best_corr = None
+    best_attempt = None
+    for _ in range(attempts):
+        noise = factorization.solve(np.random.normal(size=n))
+        nbr_means = [np.mean(noise[nbrs[i]]) for i in range(n)]
+        corr = np.corrcoef(noise, nbr_means)[0, 1]
+        if np.abs(rho - corr) < best_result:
+            best_result = np.abs(rho - corr)
+            best_corr = corr
+            best_attempt = noise
 
     # scale noise to have same variance as residuals
-    noise = noise / noise.std() * np.nanstd(x)
+    noise = best_attempt / best_attempt.std() * np.nanstd(x)
 
-    return noise, corr
+    return noise
 
 
-def moran_I(x: pd.Series, A: np.ndarray) -> float:
-    x = x.values
+def moran_I(x: np.ndarray, edge_list: np.ndarray) -> float:
+    x = x.copy()
 
-    # input the mean of x when there nan values
-    x[np.isnan(x)] = np.nanmean(x)
+    nbrs = [[] for _ in range(edge_list.shape[0])]
+    for i, j in edge_list:
+        nbrs[i].append(j)
+        nbrs[j].append(i)
 
-    # Compute mean of attribute values
-    x_bar = np.mean(x)
+    # input the neighbors
+    nbrs_means = np.array([np.nanmean(x[nbrs[i]]) for i in range(len(x))])
+    nbrs_means[np.isnan(nbrs_means)] = np.nanmean(nbrs_means)
+    x[np.isnan(x)] = nbrs_means[np.isnan(x)]
 
     # Subtract mean from attribute values
-    x_diff = x - x_bar
-
-    # Compute denominator: sum of squared differences from mean
-    denominator = np.sum(x_diff**2) + 1e-8
+    x_diff = x - x.mean()
 
     # Compute numerator: sum of product of weight and pair differences from mean
-    # Matrix multiplication to achieve vectorization
-    numerator = np.sum(A * np.outer(x_diff, x_diff))
+    src_diff = x_diff[edge_list[:, 0]]
+    dst_diff = x_diff[edge_list[:, 1]]
+    numerator = np.sum(src_diff * dst_diff) * len(x_diff)
 
-    # Compute Moran's I
-    I = len(x) / np.sum(A) * (numerator / denominator)
+    # Compute denominator: sum of squared differences from mean
+    denominator = np.sum(x_diff**2) * len(edge_list)
 
-    return float(I)
+    return float(numerator / denominator)
 
 
 def double_zip_folder(folder_path, output_path):